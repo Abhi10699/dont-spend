d91d8f4ed7e8c8b982791a45a5beb126abd71f42
Abhi10699 <patelabhi10699@gmail.com>
2024-09-23 19:48:24 -0400

    feat: added seo tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,24 @@ export default function RootLayout({
   const router = useRouter();
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8"/>
+        <title>CENTIFY - Track Your Daily Expenses and Save Money</title>
+        <meta name="description" content="CENTIFY is a minimalistic money-saving app that helps you track your daily expenses, calculate savings, and monitor your progress toward financial goals."/>
+        <meta name="keywords" content="money saver, expense tracker, financial goals, daily expenses, savings app, personal finance, budgeting, CENTIFY"/>
+        <link rel="canonical" href="https://dont-spend.vercel.com"/>
+        <meta name="author" content="Abhi Patel"/>
+        
+        <meta property="og:title" content="CENTIFY - Track Your Daily Expenses"/>
+        <meta property="og:description" content="Easily manage your daily expenses and achieve your financial goals with CENTIFY, the ultimate money-saving app."/>
+        <meta property="og:url" content="https://dont-spend.vercel.com"/>
+        <meta property="og:type" content="website"/>
+
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta name="twitter:title" content="CENTIFY - Track Your Daily Expenses"/>
+        <meta name="twitter:description" content="Manage your expenses effectively and reach your savings goals with CENTIFY."/>
+      </head>
+      
       <body className={`antialiased`}>
         <AuthProvider
           router={router}
@@ -19,6 +37,6 @@ export default function RootLayout({
           {children}
         </AuthProvider>
       </body>
-    </html>
+    </html >
   );
 }
